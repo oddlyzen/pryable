50dc8d69b4860e8f9fd5c79872ff70b7e095d5ca
Mark Coates (oddlyzen) <mark.coates@gmail.com>
2011-06-22 16:56:37 -0400

    Timing the pry session with a code block, updated README, bumped version, tagged 'pre', and releasing gem.

--- lib/pryable.rb
+++ lib/pryable.rb
@@ -1,24 +1,24 @@
 module Pryable
-  
-  begin  
-    require 'pry'  
-    module Rails  
-      class Console  
-        class IRB  
-          def self.start  
-            Pry.start  
-          end  
-        end   
-      end  
-    end  
-  rescue LoadError  
-  end
+  require 'benchmark'
+  # begin  
+  #   require 'pry'  
+  #   module Rails  
+  #     class Console  
+  #       class IRB  
+  #         def self.start  
+  #           Pry.start  
+  #         end  
+  #       end   
+  #     end  
+  #   end  
+  # rescue LoadError  
+  # end
   
   module Version
     MAJOR = 0
     MINOR = 1
-    PATCH = 3
-    BUILD = 'dev'
+    PATCH = 4
+    BUILD = 'pre'
     
     STRING = [MAJOR, MINOR, PATCH, (BUILD unless BUILD.empty?)].compact.join('.')
   end
@@ -28,16 +28,38 @@ module Pryable
   end
   
   def pry_open
-    start_time = DateTime.now
+    log_prying_session {
+      binding.pry
+    }
+  end
+  
+  def log_prying_session(&block)
     Rails.logger.info "====== Beginning Pry Session ====== Binding: #{self.inspect} ======"
-    binding.pry
-    end_time = DateTime.now
-    elapsed = end_time - start_time
-    Rails.logger.info "====== Ending Pry Session ====== Time Elapsed: #{elapsed} ======"
+    start = Time.now
+    yield
+    finish = Time.now
+    elapsed = finish.to_f - start.to_f
+    mins, secs = elapsed.divmod 60.0
+    min = mins.zero? ? "" : "#{mins.ceil}m "
+    sec = "#{secs.ceil}s"
+    time = min + sec
+    Rails.logger.info "====== Ending Pry Session ====== Session Time: #{time} ======"
   end
 
   
   module ClassMethods
-    require 'pry'
+    begin  
+      require 'pry'  
+      module Rails  
+        class Console  
+          class IRB  
+            def self.start  
+              Pry.start  
+            end  
+          end   
+        end  
+      end  
+    rescue LoadError  
+    end
   end
 end
